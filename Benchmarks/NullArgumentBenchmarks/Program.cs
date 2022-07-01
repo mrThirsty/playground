20548e15519e5955fc1c0d2a2db37f5d07d771fc
Andrew Shough <andrewshough@gmail.com>
2022-07-01 11:58:41 +0100

    Benchmarking
    Added in basic benchmark of Argument Null checks.

---
+++ Benchmarks/NullArgumentBenchmarks/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ardalis.GuardClauses;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("Runs tests to check which is quicker to check for null.");
+
+BenchmarkRunner.Run<ArgNullTests>();
+
+public class ArgNullTests
+{
+    [Benchmark]
+    public object IfStatement()
+    {
+        object test = null;
+
+        try
+        {
+            if (test is null)
+            {
+                throw new ArgumentNullException(nameof(test), "test is null");
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        return test;
+    }
+
+    [Benchmark]
+    public object ThrowIfNull()
+    {
+        object test = null;
+
+        try
+        {
+            ArgumentNullException.ThrowIfNull(test, nameof(test));
+        }
+        catch (Exception e)
+        {
+        }
+
+        return test;
+    }
+
+    [Benchmark]
+    public object ArdalisNull()
+    {
+        object test = null;
+
+        try
+        {
+            Guard.Against.Null(test, nameof(test));
+        }
+        catch (Exception e)
+        {
+        }
+        
+        return test;
+    }
+}
